01960c6787ec48fbf4182f1ce4b187ab5a0fe4bf
stereobooster <stereobooster@gmail.com>
2018-12-16 14:58:34 +0100

    first draft

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="content">
+        <form className="column">
+          <h3>Choices</h3>
+
+          <div className="field">
+            <h4>Key scheme</h4>
+            <label>
+              <input name="key-scheme" type="radio" /> Default (recommended)
+            </label>
+            <label>
+              <input name="key-scheme" type="radio" /> Synthetic
+            </label>
+            <label>
+              <input name="key-scheme" type="radio" /> Synthetic + Default
+            </label>
+          </div>
+
+          <div className="field">
+            <h4>How do you compile translations?</h4>
+            <label>
+              <input name="catlogs" type="radio" /> Catolg
+              for ecach languge e.g. <code>main.js</code> and one of <code>i18n.en.js</code>, <code>i18n.cs.js</code>
+            </label>
+            <label>
+              <input name="catlogs" type="radio" /> Separate JS bundle for each language e.g. <code>main.en.js</code> or <code>main.cs.js</code>
+            </label>
+          </div>
+
+          <div className="field">
+            <h4>What JS library do you use?</h4>
+            <label>
+              <input name="react" type="checkbox" /> React
+            </label>
+            <label>
+              <input name="plain-js" type="checkbox" /> Plain JS
+            </label>
+          </div>
+
+          <div className="field">
+            <h4>How did you configure Babel?</h4>
+            <label>
+              <input name="babel" type="radio" /> macros (recommended)
+            </label>
+            <label>
+              <input name="babel" type="radio" /> babel-transform plugin
+            </label>
+          </div>
+
+        </form>
+        <div className="column">
+          <h3>Recommendations</h3>
+        </div>
       </div>
     );
   }
